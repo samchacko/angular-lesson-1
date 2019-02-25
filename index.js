e0e52893758a54b95f63b70e0d2a3a9110dcb67e
samchacko <sam.chacko.2015@gmail.com>
2019-02-25 16:21:57 -0600

    first lesson of Angular class

---
+++ index.js
@@ -0,0 +1,3 @@
+"use strict";
+var somethingfun = "Riding a bike is not something I can do.Eating Ice Cream ...";
+console.log(somethingfun);
---
+++ index.ts
@@ -0,0 +1,2 @@
+const somethingfun: string = "Riding a bike is not something I can do.Eating Ice Cream ...";
+console.log (somethingfun);
